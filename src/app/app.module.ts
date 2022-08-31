2a042f4b8dd9537586d68bd4099e6e9ebb2d2612
D4_56536_Prajwal_Rajodiya <prajwalrajodiya24@gmail.com>
2022-08-31 16:56:59 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,65 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+// Modules
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+// Services
+import { AuthService } from './services/auth/auth.service';
+import { UserService } from './services/user/user.service';
+import { StudentService } from './services/student/student.service';
+
+// Pipes
+import { FilterPipe } from './pipes/filter.pipe';
+import { PhonePipe } from './pipes/phone.pipe';
+
+// Components
+import { AppComponent } from './components/index/app.component';
+import { StudentListComponent } from './components/student/list/student-list.component';
+import { StudentDetailsComponent } from './components/student/details/student-details.component';
+import { StudentAddComponent } from './components/student/add/student-add.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent, homeChildRoutes } from './components/home/home.component';
+import { HighlightStudentDirective } from './directives/highlight-student.directive';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+	declarations: [
+		AppComponent,
+		StudentListComponent,
+		StudentDetailsComponent,
+		StudentAddComponent,
+		LoginComponent,
+		HomeComponent,
+		FilterPipe,
+		PhonePipe,
+		HighlightStudentDirective
+	],
+	imports: [
+		BrowserModule,
+		RouterModule,
+		AppRoutingModule,
+		FormsModule,
+		ReactiveFormsModule,
+		BrowserAnimationsModule,
+		ToastrModule.forRoot({
+			timeOut: 3000,
+			positionClass: 'toast-bottom-right',
+			preventDuplicates: true,
+		}),
+	],
+	providers: [AuthService, UserService, StudentService],
+	bootstrap: [AppComponent]
 })
+
+// enableProdMode();
+
 export class AppModule { }
